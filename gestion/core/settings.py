e70f9ed76c2ed81616b1bdb1d026f40fd1803060
thiagojsdja <cuentaturqa@gmail.com>
2025-11-20 14:07:45 -0300

    Mejorando modelos 2

--- gestion/core/settings.py
+++ gestion/core/settings.py
@@ -35,7 +35,6 @@ ALLOWED_HOSTS = ['*']
 INSTALLED_APPS = [
     'jazzmin',
     'django.contrib.admin',
-    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
